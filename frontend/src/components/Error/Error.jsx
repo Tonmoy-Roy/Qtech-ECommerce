83d502cfa56b906464f0d2404fd626d853da1cb6
Tonmoy-Roy <roytonmoy901@gmail.com>
2025-07-07 20:26:48 +0600

    basic setup done

---
+++ frontend/src/components/Error/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <p className='font-bold text-3xl'>Page Not Found !</p>
+        </div>
+    );
+};
+
+export default Error;
